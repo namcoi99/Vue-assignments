fa654ae36983acf3400579ef5396ab4ebe787bcd
namnguyenn99 <namnd.vnist@gmail.com>
2021-11-05 16:04:59 +0700

    add raw FeatureSection

---
+++ src/modules/bai12/components/FeatureSection.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="feature-section">
+        <div class="feature-item">
+            <h4 class="feature-title">Clear & Usable user flows</h4>
+            <img :src="require('@/assets/images/bai12/lightbulb.svg')" alt="" />
+            <p class="feature-description">Let’s boost yout marketplace</p>
+        </div>
+        <div class="feature-item">
+            <h4 class="feature-title">Fine & Styled components</h4>
+            <img :src="require('@/assets/images/bai12/styling.svg')" alt="" />
+            <p class="feature-description">Speed up with Setproduct templates</p>
+        </div>
+        <div class="feature-item">
+            <h4 class="feature-title">Customizable & Reusable</h4>
+            <img :src="require('@/assets/images/bai12/paint.svg')" alt="" />
+            <p class="feature-description">Take our UX patterns and rebuild</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
--- src/modules/bai12/pages/ProductPage.vue
+++ src/modules/bai12/pages/ProductPage.vue
@@ -2,16 +2,19 @@
     <div class="bai12">
         <FilterSection />
         <ProductSection />
+        <FeatureSection />
     </div>
 </template>
 
 <script>
 import FilterSection from '@/modules/bai12/components/FilterSection.vue';
 import ProductSection from '@/modules/bai12/components/ProductSection.vue';
+import FeatureSection from '@/modules/bai12/components/FeatureSection.vue';
 export default {
     components: {
         FilterSection,
         ProductSection,
+        FeatureSection,
     },
 };
 </script>
